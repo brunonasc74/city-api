c9a8ef695d513ee92b248bf85dab486f0139b66f
BrunoNasc74 <bruno.hpn74@gmail.com>
2021-05-31 17:24:10 -0300

    Small fix

--- src/main/java/com/git/brunonasc/citesapi/distance/DistanceResource.java
+++ src/main/java/com/git/brunonasc/citesapi/distance/DistanceResource.java
@@ -3,6 +3,7 @@ package com.git.brunonasc.citesapi.distance;
 import com.git.brunonasc.citesapi.distance.service.DistanceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,16 +21,16 @@ public class DistanceResource {
     }
 
     @GetMapping("/by-points")
-    public Double byPoints(@RequestParam(name = "from") final Long city1,
-                           @RequestParam(name = "to") final Long city2) {
+    public ResponseEntity byPoints(@RequestParam(name = "from") final Long city1,
+                                   @RequestParam(name = "to") final Long city2) {
         log.info("byPoints");
-        return service.distanceByPointsInMiles(city1, city2);
+        return ResponseEntity.ok().body(service.distanceByPointsInMiles(city1, city2));
     }
 
     @GetMapping("/by-cube")
-    public Double byCube(@RequestParam(name = "from") final Long city1,
+    public ResponseEntity byCube(@RequestParam(name = "from") final Long city1,
                          @RequestParam(name = "to") final Long city2) {
         log.info("byCube");
-        return service.distanceByCubeInMeters(city1, city2);
+        return ResponseEntity.ok().body(service.distanceByCubeInMeters(city1, city2));
     }
 }
